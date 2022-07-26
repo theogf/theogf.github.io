ade9e6de21d2bc92d1814c73cbe23df3d45a05d9
theogf <theo.galyfajou@gmail.com>
2022-07-26 09:05:35 +0000

    Deploying to gh-pages from @ theogf/theogf.github.io@93e5a75956be33c45992b2e087171e2063f22851 🚀

--- assets/treat_profile.jl
+++ assets/treat_profile.jl
@@ -6,9 +6,7 @@ img = dither(imresize(RGB.(load("_assets/profile.png")), ratio=2/3), Bayer())
 
 dims = size(img)
 C = collect(dims .÷ 2)
-r = 15
 
-kernel(u) = exp(-u / r^2)
 kernel(u) = max(0, 70/81 * (1 - abs(u)^3)^3)
 
 indices = collect.(collect(Base.product(range.(1, dims, step=1)...)))
